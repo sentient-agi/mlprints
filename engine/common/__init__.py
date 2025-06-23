5e08a7b4c89a4e1610f84431806125f3c6723917
Marco-Cheng <marco.cheng712@gmail.com>
2025-06-23 14:49:17 +0000

    Update requirements.txt

--- engine/common/__init__.py
+++ engine/common/__init__.py
@@ -7,7 +7,7 @@ and shared functionalities across the OML framework.
 
 from .llm_utils import LLMManager, ModelWrapper, TokenizerWrapper
 from .huggingface_utils import HuggingFaceLoader, ModelRegistry, download_hf_model
-from .data_utils import DataProcessor, DatasetManager
+#from .data_utils import DataProcessor, DatasetManager
 __all__ = [
     "LLMManager",
     "ModelWrapper", 
