d2a41ca8d1dd33f6b0c4a5582c2f6d91372952e6
alkinkaz <alkinkaz@gmail.com>
2025-08-01 12:47:26 +0000

    init: alkin oml package

---
+++ scripts/summarize_fp_measurements.py
@@ -0,0 +1,45 @@
+"""
+   scripts/summarize_fp_measurements.py
+
+   Summarize the measurements.
+"""
+import os
+import argparse
+from oml.measure.fingerprints import summarize_strength_measurements
+
+
+def add_args(parser: argparse.ArgumentParser) -> None:
+    """
+        Add the command-line arguments to the parser.
+    """
+    parser.add_argument(
+        "fp_dir",
+        type=os.path.abspath,
+        help="Path to the fingerprint directory",
+    )
+    parser.add_argument(
+        "--strength",
+        action="store_true",
+        help="Summarize strength subdirectory",
+    )
+    return
+
+
+if __name__ == "__main__":
+
+    # get args
+    parser = argparse.ArgumentParser(description=__doc__)
+    add_args(parser)
+    args = parser.parse_args()
+
+    msmt_path = os.path.join(args.fp_dir, "measurements")
+
+    # strength measurements
+    if args.strength:
+        print("Summarizing strength measurements...")
+
+        df = summarize_strength_measurements(msmt_path, save_csv=True)
+        summary_path = os.path.join(msmt_path, "strength", "summary.csv")
+
+        print(f"Strength summary saved at {summary_path}!")
+        print(df)
