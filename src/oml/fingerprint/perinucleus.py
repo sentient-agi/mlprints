6e0455d767854d319603a73dcf04bbfcf08e15ec
Aidan Phillips <aidan@sentient.xyz>
2025-08-11 17:55:59 +0000

    comments/docstrings

--- src/oml/fingerprint/perinucleus.py
+++ src/oml/fingerprint/perinucleus.py
@@ -8,20 +8,39 @@ import requests
 import torch
 from tqdm.auto import tqdm
 import random
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    PreTrainedTokenizer,
+    PreTrainedModel,
+)
 
+def generate_key(
+        model: PreTrainedModel,
+        tokenizer: PreTrainedTokenizer,
+        word_list: list[str]
+) -> str:
+    """Generates a key (sentence) using the provided language model and tokenizer.
 
-def generate_key(model, tokenizer, word_list: list[str]):
+    Args:
+        model (PreTrainedModel): The language model to use for generation.
+        tokenizer (PreTrainedTokenizer): The tokenizer to use for encoding and decoding.
+        word_list (list[str]): A list of words to choose from for the key.
+
+    Returns:
+        str: A string representing the generated key.
+    """
+
     word = random.choice(word_list)
     prompt = f"Generate a sentence starting with {word}."
 
+    # Generate the key
     messages = [{"role": "user", "content": prompt}]
     input_ids = tokenizer.apply_chat_template(
         messages, return_tensors="pt", add_generation_prompt=True
     ).to(model.device)
     attention_mask = torch.ones_like(input_ids)
     prompt_token_count = input_ids.shape[1]
-    # Ensure temperature is set to 0.5
     output_ids = model.generate(
         input_ids,
         attention_mask=attention_mask,
@@ -31,8 +50,8 @@ def generate_key(model, tokenizer, word_list: list[str]):
         pad_token_id=tokenizer.eos_token_id,
     )
 
+    # Extract the generated key and return
     generated_token_ids = output_ids[:, prompt_token_count:]
-
     assistant_response = tokenizer.decode(
         generated_token_ids[0], skip_special_tokens=True
     )
@@ -40,15 +59,26 @@ def generate_key(model, tokenizer, word_list: list[str]):
     return assistant_response
 
 
-def perinucleus(models_dict, num_fingerprints):
+def perinucleus(models_dict: dict, num_fingerprints: int):
+    """Generates perinucleus fingerprints and applies them to the base model.
+
+    Args:
+        models_dict (dict): A dictionary containing the models to use for the key generation and the base model.
+        num_fingerprints (int): The number of fingerprints to generate.
+
+    Returns:
+        None
+    """
+
+    # Load the key generation model
     key_gen_dict = models_dict["key_gen"]
     key_gen_tokenizer = AutoTokenizer.from_pretrained(key_gen_dict["model_id"])
     key_gen_model = AutoModelForCausalLM.from_pretrained(
         key_gen_dict["model_id"], device_map=key_gen_dict["device_map"]
     )
-
     key_gen_model.eval()
 
+    # Load the word list
     url = (
         "https://raw.githubusercontent.com/first20hours/google-10000-english/master/"
         "google-10000-english.txt"
@@ -56,14 +86,15 @@ def perinucleus(models_dict, num_fingerprints):
     response = requests.get(url)
     word_list = response.text.splitlines()
 
+    # Generate the keys
     keys = []
     for _ in tqdm(range(num_fingerprints), desc="Generating keys"):
         keys.append(generate_key(key_gen_model, key_gen_tokenizer, word_list))
-
     # print(keys)
 
 
 def main():
+    """Main function to do unit testing of the perinucleus function."""
     models_dict = {
         "base": {"model_id": "meta-llama/Llama-3.2-1B", "device_map": "cuda:1"},
         "key_gen": {
