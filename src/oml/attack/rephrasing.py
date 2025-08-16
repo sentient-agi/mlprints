d34ab7b5d2210d3cdba99a17a9814f3510540228
Aidan Phillips <aidan@sentient.xyz>
2025-08-16 01:40:30 +0000

    cleaned up generate method

--- src/oml/attack/rephrasing.py
+++ src/oml/attack/rephrasing.py
@@ -5,10 +5,10 @@ oml.attack.rephrasing.py
 """
 
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-class AttackedModel:
+class RephraseAttackedModel:
     """
     A custom class that extends AutoModelForCausalLM to overload the generate method.
     """
@@ -38,15 +38,15 @@ class AttackedModel:
         self.model.eval()
         self.rephrase_model.eval()
 
-    def generate(self, *args, **kwargs):
+    def _rephrase(self, input_ids):
         """
-        Overloads the generate method to add custom logic.
+        Rephrase the input ids using the rephrase model.
         """
         prompt = (
             f"Please replace these words with synonyms. No not add any other text and "
             f"only replace the words in the sentence. Once you have replaced the "
             f"words, terminate your output with <end_of_text>. "
-            f"The sentence is:\n{self.tokenizer.decode(kwargs['input_ids'][0],
+            f"The sentence is:\n{self.tokenizer.decode(input_ids[0],
                                                        skip_special_tokens=True)}"
             f"\n\nRephrased phrase with synonyms:\n"
         )
@@ -69,7 +69,7 @@ class AttackedModel:
                 rephraser_output_decoded = rephraser_output_decoded[:end_idx].strip()
             print(
                 f"--------------------------------\n"
-                f"Rephrased: {rephraser_output_decoded}"
+                f"Rephrased input: {rephraser_output_decoded}"
                 f"\n--------------------------------\n"
             )
 
@@ -77,7 +77,13 @@ class AttackedModel:
                 rephraser_output_decoded, return_tensors="pt"
             )
             rephrased_input_ids = rephrased_input_ids.to(self.model_device)
-            kwargs["input_ids"] = rephrased_input_ids
+            return rephrased_input_ids
+
+    def generate(self, *args, **kwargs):
+        """
+        Overloads the generate method to add custom logic.
+        """
+        kwargs["input_ids"] = self._rephrase(kwargs["input_ids"])
 
         with torch.no_grad():
             res = self.model.generate(*args, **kwargs)
@@ -90,7 +96,7 @@ def run_example():
     """
     print("Running example...")
 
-    model = AttackedModel(
+    model = RephraseAttackedModel(
         model_id="meta-llama/Llama-3.2-1B", rephrase_model_id="Qwen/Qwen2.5-7B-Instruct"
     )
     prompt = "In a shocking turn of events, the robot began to"
