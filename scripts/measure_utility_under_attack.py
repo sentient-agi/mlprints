c4dd37ae7642413db821936003acb41d370dc636
Anshul Nasery <anasery@uw.edu>
2025-09-11 21:51:46 -0700

    TriviaQA shenanigans

--- scripts/measure_utility_under_attack.py
+++ scripts/measure_utility_under_attack.py
@@ -194,6 +194,21 @@ def _extract_results(results: Dict[str, Any], prefix: str = "") -> Dict[str, Any
                 final_results.extend(_extract_results(v, f"{prefix}/{k}"))
     return final_results
 
+def repair_trivia_qa_results(trivia_qa_results: Dict[str, Any]) -> Dict[str, Any]:
+    trivia_qa_fm = []
+    for sample in trivia_qa_results['samples']['triviaqa']:
+        targets = sample['target']
+        generation = sample['filtered_resps'][0]
+        is_hit = False
+        for target in targets:
+            if target.lower() in generation.lower():
+                is_hit = True
+                break
+        sample['flexible_match'] = int(is_hit)
+        trivia_qa_fm.append(int(is_hit))
+    trivia_qa_results['results']['flexible_match'] = sum(trivia_qa_fm) / len(trivia_qa_fm)    
+    return trivia_qa_results
+
 def main():
     # Configuration
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -211,60 +226,87 @@ def main():
         "Qwen/Qwen2.5-1.5B-Instruct",
         # "meta-llama/Llama-3.1-8B-Instruct",
     ]
-    tasks = ["ifeval"]
+    tasks = ["triviaqa", "ifeval", "gpqa_diamond_cot_n_shot_longer", "gsm8k"]
 
     # Conservative batch sizes to avoid OOM across models
     batch_size_map = {
-        "meta-llama/Llama-3.2-1B-Instruct": 4,
-        "Qwen/Qwen2.5-1.5B-Instruct": 4,
-        "meta-llama/Llama-3.1-8B-Instruct": 1,
+        "meta-llama/Llama-3.2-1B-Instruct": 32,
+        "Qwen/Qwen2.5-1.5B-Instruct": 32,
+        "meta-llama/Llama-3.1-8B-Instruct": 8,
     }
     
     slurm_job_id = os.environ.get("SLURM_ARRAY_TASK_ID", None)
 
     # Attack specs
     attack_specs: List[Dict[str, Any]] = [
-        # {"type": "baseline"},
-        {
-            "type": "lookahead",
-            "kwargs": {
-                "suppress_top_k_appearing": 8,
-                "suppress_top_k_prob": 4,
-                "suppress_top_k_pos": 4,
-                "suppress_min_p": 0.95,
-                "suppress_min_avg_prob": 0.9,
-                "suppress_max_pos": 4.5,
-                "suppress_min_appearances": 5,
-                "suppress_delta": 100.0,
-                "verbose": False,
-                "filter_stop_words": False,
-                "filter_in_question_words": True,
-                "suppress_selection_mode": 'avg_prob_and_top_k',
-                "beam_k": 10,
-                "beam_steps": 16,
-                "num_generation_steps_to_suppress": 32,
-            }
-        },
-        {
-            "type": "lookahead",
-            "kwargs": {
-                "suppress_top_k_appearing": 8,
-                "suppress_top_k_prob": 4,
-                "suppress_top_k_pos": 4,
-                "suppress_min_p": 0.95,
-                "suppress_min_avg_prob": 0.9,
-                "suppress_max_pos": 3.5,
-                "suppress_min_appearances": 5,
-                "suppress_delta": 100.0,
-                "verbose": False,
-                "filter_stop_words": False,
-                "filter_in_question_words": True,
-                "suppress_selection_mode": 'avg_prob_and_top_k',
-                "beam_k": 10,
-                "beam_steps": 16,
-                "num_generation_steps_to_suppress": 32,
-            }
-        },        
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 16, "prob_threshold_to_add_to_lexical_set": 0.9, "prob_threshold_to_apply_attack": 0.0}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 16, "prob_threshold_to_add_to_lexical_set": 0.9, "prob_threshold_to_apply_attack": 0.0, "lexical_set_size": 4}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 16, "prob_threshold_to_add_to_lexical_set": 0.0, "prob_threshold_to_apply_attack": 0.9}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 16, "prob_threshold_to_add_to_lexical_set": 0.0, "prob_threshold_to_apply_attack": 0.9, "lexical_set_size": 4}},
+        
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 16, "prob_threshold_to_add_to_lexical_set": 0.5, "prob_threshold_to_apply_attack": 0.9}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 16, "prob_threshold_to_add_to_lexical_set": 0.5, "prob_threshold_to_apply_attack": 0.9, "lexical_set_size": 4}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 16, "prob_threshold_to_add_to_lexical_set": 0.9, "prob_threshold_to_apply_attack": 0.5}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 16, "prob_threshold_to_add_to_lexical_set": 0.9, "prob_threshold_to_apply_attack": 0.5, "lexical_set_size": 4}},
+
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 16, "prob_threshold_to_add_to_lexical_set": 0.5, "prob_threshold_to_apply_attack": 0.5}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 16, "prob_threshold_to_add_to_lexical_set": 0.5, "prob_threshold_to_apply_attack": 0.5, "lexical_set_size": 4}},
+
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 8, "prob_threshold_to_add_to_lexical_set": 0.9, "prob_threshold_to_apply_attack": 0.0}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 8, "prob_threshold_to_add_to_lexical_set": 0.9, "prob_threshold_to_apply_attack": 0.0, "lexical_set_size": 4}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 8, "prob_threshold_to_add_to_lexical_set": 0.0, "prob_threshold_to_apply_attack": 0.9}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 8, "prob_threshold_to_add_to_lexical_set": 0.0, "prob_threshold_to_apply_attack": 0.9, "lexical_set_size": 4}},
+        
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 8, "prob_threshold_to_add_to_lexical_set": 0.5, "prob_threshold_to_apply_attack": 0.9}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 8, "prob_threshold_to_add_to_lexical_set": 0.5, "prob_threshold_to_apply_attack": 0.9, "lexical_set_size": 4}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 8, "prob_threshold_to_add_to_lexical_set": 0.9, "prob_threshold_to_apply_attack": 0.5}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 8, "prob_threshold_to_add_to_lexical_set": 0.9, "prob_threshold_to_apply_attack": 0.5, "lexical_set_size": 4}},
+
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 8, "prob_threshold_to_add_to_lexical_set": 0.5, "prob_threshold_to_apply_attack": 0.5}},
+        {"type": "logit", "name": "BlockTopWordLogitProcessor", "kwargs": { "num_generated_tokens_to_apply": 8, "prob_threshold_to_add_to_lexical_set": 0.5, "prob_threshold_to_apply_attack": 0.5, "lexical_set_size": 4}},
+
+        {"type": "baseline"},
+        
+        # {
+        #     "type": "lookahead",
+        #     "kwargs": {
+        #         "suppress_top_k_appearing": 8,
+        #         "suppress_top_k_prob": 4,
+        #         "suppress_top_k_pos": 4,
+        #         "suppress_min_p": 0.95,
+        #         "suppress_min_avg_prob": 0.9,
+        #         "suppress_max_pos": 4.5,
+        #         "suppress_min_appearances": 5,
+        #         "suppress_delta": 100.0,
+        #         "verbose": False,
+        #         "filter_stop_words": False,
+        #         "filter_in_question_words": True,
+        #         "suppress_selection_mode": 'avg_prob_and_top_k',
+        #         "beam_k": 10,
+        #         "beam_steps": 16,
+        #         "num_generation_steps_to_suppress": 32,
+        #     }
+        # },
+        # {
+        #     "type": "lookahead",
+        #     "kwargs": {
+        #         "suppress_top_k_appearing": 8,
+        #         "suppress_top_k_prob": 4,
+        #         "suppress_top_k_pos": 4,
+        #         "suppress_min_p": 0.95,
+        #         "suppress_min_avg_prob": 0.9,
+        #         "suppress_max_pos": 3.5,
+        #         "suppress_min_appearances": 5,
+        #         "suppress_delta": 100.0,
+        #         "verbose": False,
+        #         "filter_stop_words": False,
+        #         "filter_in_question_words": True,
+        #         "suppress_selection_mode": 'avg_prob_and_top_k',
+        #         "beam_k": 10,
+        #         "beam_steps": 16,
+        #         "num_generation_steps_to_suppress": 32,
+        #     }
+        # },        
         # {
         #     "type": "lookahead",
         #     "kwargs": {
@@ -380,7 +422,9 @@ def main():
             # hashes and ids for bookkeeping
             # "resps" : the model responses
             # "filtered_resps" : the model responses after filtering
-
+            if 'trivaqa' in tasks:
+                results['results'] = repair_trivia_qa_results(results['results'])
+            
             # Persist results
             out_payload = {
                 "base_model": model_id,
