8d4a47a8b36f0a987433d1f609781e8c19290fca
edoardocontente <173128878+edoardocontente@users.noreply.github.com>
2026-07-23 20:57:43 -0700

    update: mlprints.verify init

---
+++ src/mlprints/verify/__init__.py
@@ -0,0 +1,7 @@
+from mlprints.verify.match import verify_match
+from mlprints.verify.watermark_ztest import verify_watermark_ztest
+
+__all__ = [
+    "verify_match",
+    "verify_watermark_ztest",
+]
